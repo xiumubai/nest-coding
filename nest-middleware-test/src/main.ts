b7b57790fd751efc76d526c75c4b7b7d0e2845bf
guojianli <1547702880@qq.com>
2024-09-14 12:07:23 +0800

    feat: middleware

--- nest-middleware-test/src/main.ts
+++ nest-middleware-test/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(function (req, res, next) {
+    console.log('brefore');
+    next();
+    console.log('after');
+  });
   await app.listen(3000);
 }
 bootstrap();
